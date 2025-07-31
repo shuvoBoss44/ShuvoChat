9f9962564e8fdf6976edd63ef7da1f3a93b9aea4
shuvoBoss44 <chakmashuvo2016@gmail.com>
2025-08-01 01:24:34 +0600

    changes the backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://shuvochat-backend.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
