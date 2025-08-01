805da09067af59536e3b92212a3baad7476015fd
shuvoBoss44 <chakmashuvo2016@gmail.com>
2025-08-01 17:39:04 +0600

    video call button

--- src/components/CustomChannelHeader.jsx
+++ src/components/CustomChannelHeader.jsx
@@ -19,7 +19,7 @@ const CustomChannelHeader = ({ handleVideoCall }) => {
         </button>
         <ChannelHeader className="text-lg font-semibold text-gray-900 dark:text-gray-100" />
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center mr-2">
         <CallButton handleVideoCall={handleVideoCall} />
       </div>
     </div>
