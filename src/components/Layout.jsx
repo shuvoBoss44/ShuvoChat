63eae94641fe6db1279a1681d7ee4adae6097976
shuvoBoss44 <chakmashuvo2016@gmail.com>
2025-08-01 16:33:58 +0600

    fixed many things

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,11 +8,11 @@ const Layout = ({ showSidebar = true, children }) => {
       {showSidebar && <Sidebar />}
       <div
         className={`flex-1 flex flex-col ${
-          showSidebar ? "lg:ml-64" : ""
-        } transition-all duration-300`}
+          showSidebar ? "md:ml-48 lg:ml-64" : ""
+        } transition-all duration-300 max-w-full`}
       >
         <Header />
-        <main className="flex-1 p-4 sm:p-6 md:p-8">{children}</main>
+        <main className="flex-1 p-4 sm:p-2 md:p-8 lg:p-10">{children}</main>
       </div>
     </div>
   );
